7559b566487eb772193d61d07f2ee6d6e1cfd8c4
dedNewGuy <aminkafrinewgen@gmail.com>
2025-06-10 01:30:47 +0800

    camera movement (no rotation yet)

--- src/com/notearth/main/Renderer.java
+++ src/com/notearth/main/Renderer.java
@@ -6,12 +6,31 @@ import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
 import com.jogamp.opengl.fixedfunc.GLMatrixFunc;
 import com.jogamp.opengl.glu.GLU;
+import com.notearth.inputHandler.InputHandler;
 
 public class Renderer implements GLEventListener {
 
+    private static class Camera {
+        public float x, y, z;
+        public float speed;
+        public Camera(float x, float y, float z, float speed) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.speed = speed;
+        }
+    }
+
     private GLU glu;
+    private final InputHandler inputHandler;
+    private Camera camera;
 
     private float angle = 0;
+
+    public Renderer(InputHandler input) {
+        this.inputHandler = input;
+        camera = new Camera(0.0f, 0.0f, -6.0f, 0.2f);
+    }
 
     @Override
     public void init(GLAutoDrawable drawable) {
@@ -31,7 +50,9 @@ public class Renderer implements GLEventListener {
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);
         gl.glLoadIdentity(); // Reset model view matrix
 
-        gl.glTranslatef(0.0f, 0.0f, -6.0f);
+        input();
+
+        gl.glTranslatef(camera.x, camera.y, camera.z);
         gl.glRotatef(angle, 0.0f, 0.0f, 1.0f);
         gl.glBegin(GL.GL_TRIANGLES);
         gl.glVertex3f(1.0f, -1.0f, 0.0f);
@@ -40,6 +61,7 @@ public class Renderer implements GLEventListener {
         gl.glEnd();
 
         angle += 1f;
+
     }
 
     @Override
@@ -57,5 +79,27 @@ public class Renderer implements GLEventListener {
 
         gl.glMatrixMode(GLMatrixFunc.GL_MODELVIEW);
         gl.glLoadIdentity();
+
+    }
+
+    private void input() {
+        if (this.inputHandler.isWPressed) {
+            camera.z += camera.speed;
+        }
+        if (this.inputHandler.isAPressed) {
+            camera.x += camera.speed;
+        }
+        if (this.inputHandler.isSPressed) {
+            camera.z += -camera.speed;
+        }
+        if (this.inputHandler.isDPressed) {
+            camera.x += -camera.speed;
+        }
+        if (this.inputHandler.isSpacePressed) {
+            camera.y += -camera.speed;
+        }
+        if (this.inputHandler.isShiftPressed) {
+            camera.y += camera.speed;
+        }
     }
 }
