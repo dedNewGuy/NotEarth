2dae53088d2a6206355708a8dc56bb17f2093805
dedNewGuy <aminkafrinewgen@gmail.com>
2025-06-10 12:34:20 +0800

    render 3D cube

--- src/com/notearth/main/Renderer.java
+++ src/com/notearth/main/Renderer.java
@@ -8,6 +8,11 @@ import com.jogamp.opengl.fixedfunc.GLMatrixFunc;
 import com.jogamp.opengl.glu.GLU;
 import com.notearth.inputHandler.InputHandler;
 
+import static com.jogamp.opengl.GL.GL_DEPTH_BUFFER_BIT;
+import static com.jogamp.opengl.GL.GL_NICEST;
+import static com.jogamp.opengl.GL2ES1.GL_PERSPECTIVE_CORRECTION_HINT;
+import static com.jogamp.opengl.fixedfunc.GLLightingFunc.GL_SMOOTH;
+
 public class Renderer implements GLEventListener {
 
     private static class Camera {
@@ -25,8 +30,6 @@ public class Renderer implements GLEventListener {
     private final InputHandler inputHandler;
     private final Camera camera;
 
-    private float angle = 0;
-
     public Renderer(InputHandler input) {
         this.inputHandler = input;
         camera = new Camera(0.0f, 0.0f, -6.0f, 0.2f);
@@ -37,6 +40,12 @@ public class Renderer implements GLEventListener {
         glu = new GLU();
         GL2 gl = drawable.getGL().getGL2();
         gl.glClearColor(0.3f, 0.45f, 0.1f, 0.0f);
+        gl.glClearDepth(1.0f);
+        gl.glEnable(GL.GL_DEPTH_TEST);
+        gl.glEnable(GL.GL_CULL_FACE);
+        gl.glDepthFunc(GL.GL_LEQUAL);
+        gl.glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+        gl.glShadeModel(GL_SMOOTH);
     }
 
     @Override
@@ -47,20 +56,75 @@ public class Renderer implements GLEventListener {
     @Override
     public void display(GLAutoDrawable drawable) {
         GL2 gl = drawable.getGL().getGL2();
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         gl.glLoadIdentity(); // Reset model view matrix
 
         input();
 
         gl.glTranslatef(camera.x, camera.y, camera.z);
-        gl.glRotatef(angle, 0.0f, 0.0f, 1.0f);
         gl.glBegin(GL.GL_TRIANGLES);
-        gl.glVertex3f(1.0f, -1.0f, 0.0f);
-        gl.glVertex3f(-1.0f, -1.0f, 0.0f);
-        gl.glVertex3f(0.0f, 1.0f, 0.0f);
-        gl.glEnd();
+        // FRONT FACE
+        gl.glColor3f(0.0f, 0.3f, 0.0f);
+        gl.glVertex3f(1.0f, 1.0f, 1.0f);
+        gl.glVertex3f(-1.0f, -1.0f, 1.0f);
+        gl.glVertex3f(1.0f, -1.0f, 1.0f);
 
-        angle += 1f;
+        gl.glVertex3f(-1.0f, 1.0f, 1.0f);
+        gl.glVertex3f(-1.0f, -1.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.0f, 1.0f);
+
+
+        // BACK FACE
+        gl.glColor3f(0.4f, 0.3f, 0.0f);
+        gl.glVertex3f(1.0f, 1.0f, -1.0f);
+        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+        gl.glVertex3f(1.0f, -1.0f, -1.0f);
+
+        gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+        gl.glVertex3f(1.0f, 1.0f, -1.0f);
+
+        // RIGHT FACE
+        gl.glColor3f(0.4f, 0.0f, 0.0f);
+        gl.glVertex3f(1.0f, -1.0f, -1.0f);
+        gl.glVertex3f(1.0f, 1.0f, -1.0f);
+        gl.glVertex3f(1.0f, -1.0f, 1.0f);
+
+        gl.glVertex3f(1.0f, -1.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.0f, -1.0f);
+        gl.glVertex3f(1.0f, 1.0f, 1.0f);
+
+        // LEFT FACE
+        gl.glColor3f(0.9f, 0.5f, 0.5f);
+        gl.glVertex3f(-1.0f, -1.0f, 1.0f);
+        gl.glVertex3f(-1.0f, 1.0f, 1.0f);
+        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+
+        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+        gl.glVertex3f(-1.0f, 1.0f, 1.0f);
+        gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+
+        // TOP FACE
+        gl.glColor3f(0.0f, 0.0f, 0.5f);
+        gl.glVertex3f(1.0f, 1.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.0f, -1.0f);
+        gl.glVertex3f(-1.0f, 1.0f, 1.0f);
+
+        gl.glVertex3f(-1.0f, 1.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.0f, -1.0f);
+        gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+
+        // BOTTOM FACE
+        gl.glColor3f(0.0f, 0.5f, 0.5f);
+        gl.glVertex3f(1.0f, -1.0f, -1.0f);
+        gl.glVertex3f(1.0f, -1.0f, 1.0f);
+        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+
+        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+        gl.glVertex3f(1.0f, -1.0f, 1.0f);
+        gl.glVertex3f(-1.0f, -1.0f, 1.0f);
+
+        gl.glEnd();
 
     }
 
