b6051af6a64e047a738bf25b550562d3ea1bfe66
dedNewGuy <aminkafrinewgen@gmail.com>
2025-06-13 01:25:35 +0800

    calculate vertex normals

--- src/main/java/com/notearth/mesh/Plane.java
+++ src/main/java/com/notearth/mesh/Plane.java
@@ -2,15 +2,15 @@ package com.notearth.mesh;
 
 import com.jogamp.opengl.math.Vec3f;
 
-import java.util.Arrays;
 
 public class Plane {
 
-    private Vec3f position;
-    private float width, height;
-    private int segment;
+    private final Vec3f position;
+    private final float width;
+    private final float height;
+    private final int segment;
 
-    private Mesh mesh;
+    private final Mesh mesh;
 
     public Plane(Vec3f position, float width, float height, int segment) {
         this.position = position.copy();
@@ -45,7 +45,7 @@ public class Plane {
                 float v = 1 - (textureOffset * row);
 
                 float nx = 0.0f;
-                float ny = 1.0f;
+                float ny = 0.0f;
                 float nz = 0.0f;
 
                 vertexData[vertexDataIndex] = x;
@@ -85,11 +85,68 @@ public class Plane {
             }
         }
 
+        calculateVertexNormals(vertexData, indices);
+
         return new Mesh(vertexData, indices);
+    }
+
+    public void calculateVertexNormals(float[] vertexData, int[] indices) {
+        int idx;
+        for (int i = 0; i < indices.length; i += 3) {
+
+            int v1Index = indices[i];
+            int v2Index = indices[i + 1];
+            int v3Index = indices[i + 2];
+
+            idx = v1Index * 8;
+            Vec3f a = new Vec3f(vertexData[idx], vertexData[idx + 1], vertexData[idx + 2]);
+
+            idx = v2Index * 8;
+            Vec3f b = new Vec3f(vertexData[idx], vertexData[idx + 1], vertexData[idx + 2]);
+
+            idx = v3Index * 8;
+            Vec3f c = new Vec3f(vertexData[idx], vertexData[idx + 1], vertexData[idx + 2]);
+
+            Vec3f faceNormals = computeFaceNormals(a.copy(), b.copy(), c.copy());
+
+            idx = v1Index * 8;
+            vertexData[idx + 5] += faceNormals.x();
+            vertexData[idx + 6] += faceNormals.y();
+            vertexData[idx + 7] += faceNormals.z();
+
+            idx = v2Index * 8;
+            vertexData[idx + 5] += faceNormals.x();
+            vertexData[idx + 6] += faceNormals.y();
+            vertexData[idx + 7] += faceNormals.z();
+
+            idx = v3Index * 8;
+            vertexData[idx + 5] += faceNormals.x();
+            vertexData[idx + 6] += faceNormals.y();
+            vertexData[idx + 7] += faceNormals.z();
+        }
+
+        for (int i = 0; i < vertexData.length; i += 8) {
+
+            Vec3f vertexNormal = new Vec3f(vertexData[i + 5], vertexData[i + 6], vertexData[i + 7]);
+            vertexNormal.normalize();
+
+
+            vertexData[i + 5] = vertexNormal.x();
+            vertexData[i + 6] = vertexNormal.y();
+            vertexData[i + 7] = vertexNormal.z();
+        }
+    }
+
+    private Vec3f computeFaceNormals(Vec3f a, Vec3f b, Vec3f c) {
+        return b.sub(a).cross(c.sub(a));
     }
 
     public Vec3f getPosition() {
         return position;
+    }
+
+    public int getSegment() {
+        return segment;
     }
 
     public Mesh getMesh() {
